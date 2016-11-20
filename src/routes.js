9e22439b2b22037ddde96fdd4cff1a1666b5a9c7
blooies <joy28@msn.com>
2016-11-20 01:45:27 -0500

    added more routing

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,9 @@ import NotFound from './components/NotFound';
 const Routes = (props) => (
     <Router {...props}>
         <Route path='/' component={App} />
+        <Route path='/images' component={App} />
+        <Route path='/people' component={App} />
+        <Route path='/exhibitions' component={App} />
         <Route path='/about' component={About} />
         <Route path='*' component={NotFound} />
     </Router>
