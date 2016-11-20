4759292cbad018c39b3eed13c6e0f91760703f2f
Johnathan Weisner <joweisner@gmail.com>
2016-11-19 23:33:46 -0500

    Use tab component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -3,14 +3,18 @@ import classnames from 'classnames';
 
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import {Tabs, Tab} from 'material-ui/Tabs';
 import FontIcon from 'material-ui/FontIcon';
-import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 
 import './style.css';
 
-const personIcon = <FontIcon className="material-icons">perm_identity</FontIcon>;
-const imagesIcon = <FontIcon className="material-icons">filter</FontIcon>;
-const exhibitionsIcon = <FontIcon className="material-icons">rowing</FontIcon>;
+const makeIcon = (name) => {
+    return (<FontIcon className="material-icons">{name}</FontIcon>);
+}
+const allIcon = makeIcon('all_inclusive');
+const peopleIcon = makeIcon('perm_identity');
+const imagesIcon = makeIcon('filter');
+const exhibitionsIcon = makeIcon('rowing');
 
 const style = {
     margin: 12
@@ -35,23 +39,28 @@ export default class SearchBar extends Component {
                     primary={true}
                     style={style} 
                 />
-                <BottomNavigation selectedIndex={this.state.selectedIndex}>
-                    <BottomNavigationItem
-                        label="Images"
-                        icon={imagesIcon}
-                        onTouchTap={() => this.select(0)}
+                <Tabs selectedIndex={this.state.selectedIndex}>
+                    <Tab
+                      icon={allIcon}
+                      label="ALL"
+                      onTouchTap={() => this.select(0)}
                     />
-                    <BottomNavigationItem
-                        label="Person"
-                        icon={personIcon}
-                        onTouchTap={() => this.select(1)}
+                    <Tab
+                      icon={imagesIcon}
+                      label="IMAGES"
+                      onTouchTap={() => this.select(1)}
                     />
-                    <BottomNavigationItem
-                        label="Exhibitions"
-                        icon={exhibitionsIcon}
-                        onTouchTap={() => this.select(2)}
-                  />
-                </BottomNavigation>
+                    <Tab
+                      icon={peopleIcon}
+                      label="PEOPLE"
+                      onTouchTap={() => this.select(2)}
+                    />
+                    <Tab
+                      icon={exhibitionsIcon}
+                      label="EXHIBITIONS"
+                      onTouchTap={() => this.select(3)}
+                    />
+                </Tabs>
             </div>
         );
     }
